f52b01cca7acd06c2f05e3fc7eae244c1cce4be5
Martin Alejandro Viscarra <viscarramartin23@gmail.com>
2026-06-30 20:00:40 -0300

    feat: Load Balancing
    Implementación del patrón Load Balancing en Product-Service y Cart-service
    - Agregado de la dependencia spring-cloud-starter-loadbalancer en los archivos pom.xml de ambos proyectos.
    -Se agrega a application.propertieslas (Product-Service) las propiedades:
    erver.port=0
    eureka.instance.instance-id=${spring.application.name}:${random.value}
    Para poder levantar más de una instancia de este servicio a la vez y que cada una reciba un número de puerto único
    
    - El método getProductsByIds de ProductController muestra un log con el número de puerto de la instancia que está recibiendo la solicitud http.

--- product-service/src/main/java/com/martinviscarra/microservices/project/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/martinviscarra/microservices/project/product_service/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.martinviscarra.microservices.project.product_service.controller;
 import com.martinviscarra.microservices.project.product_service.dto.ProductDto;
 import com.martinviscarra.microservices.project.product_service.dto.ProductRequestDto;
 import com.martinviscarra.microservices.project.product_service.service.IProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,40 +20,49 @@ public class ProductController {
 
     private final IProductService productService;
 
+    @Value("${server.port}")
+    private int serverPort;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductDto create(@Valid @RequestBody ProductRequestDto request){
+    public ProductDto create(@Valid @RequestBody ProductRequestDto request) {
         return productService.save(request);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ProductDto edit(@Valid @RequestBody ProductRequestDto request, @PathVariable Long id){
+    public ProductDto edit(@Valid @RequestBody ProductRequestDto request, @PathVariable Long id) {
         return productService.edit(id, request);
     }
 
     @PatchMapping("/{id}/activate")
     @ResponseStatus(HttpStatus.OK)
-    public ProductDto activate(@PathVariable Long id){
+    public ProductDto activate(@PathVariable Long id) {
         return productService.activate(id);
     }
 
     @PatchMapping("/{id}/deactivate")
     @ResponseStatus(HttpStatus.OK)
-    public ProductDto deactivate(@PathVariable Long id){
+    public ProductDto deactivate(@PathVariable Long id) {
         return productService.deactivate(id);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ProductDto getById(@PathVariable Long id){
+    public ProductDto getById(@PathVariable Long id) {
         return productService.getById(id);
     }
 
     @GetMapping("/batch")
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductDto> getProductsByIds(@RequestParam("ids") List<Long> ids){
-        return productService.getProductsByIds(ids);
+    public List<ProductDto> getProductsByIds(@RequestParam("ids") List<Long> ids, HttpServletRequest request) {
+
+        List<ProductDto> response = productService.getProductsByIds(ids);
+
+        // request.getLocalPort() consulta el número de puerto real que está usando esta instancia
+        System.out.println("----- Datos de productos servidos desde la instancia con puerto " + request.getLocalPort() + " -----");
+
+        return response;
     }
 
 }
